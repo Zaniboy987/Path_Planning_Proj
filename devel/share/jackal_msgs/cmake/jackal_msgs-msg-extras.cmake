80defd74239121a39a28c75dfcc718061920873f
Zaniboy987 <98477740+Zaniboy987@users.noreply.github.com>
2025-04-29 14:15:01 -0400

    Delete devel directory

--- devel/share/jackal_msgs/cmake/jackal_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(jackal_msgs_MESSAGE_FILES "/home/anazar1/catkin_ws/src/jackal/jackal_msgs/msg/Drive.msg;/home/anazar1/catkin_ws/src/jackal/jackal_msgs/msg/DriveFeedback.msg;/home/anazar1/catkin_ws/src/jackal/jackal_msgs/msg/Feedback.msg;/home/anazar1/catkin_ws/src/jackal/jackal_msgs/msg/Status.msg")
-set(jackal_msgs_SERVICE_FILES "")
