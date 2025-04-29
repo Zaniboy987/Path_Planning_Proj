585635523bc56c4f5d37dccf75c38038901b78b0
Zaniboy987 <98477740+Zaniboy987@users.noreply.github.com>
2025-04-29 14:14:52 -0400

    Delete build directory

--- build/jackal/jackal_description/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anazar1/catkin_ws/src/jackal/jackal_description
-# Build directory: /home/anazar1/catkin_ws/build/jackal/jackal_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(_ctest_jackal_description_roslaunch-check_launch_description.launch "/home/anazar1/catkin_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python3" "/opt/ros/noetic/share/catkin/cmake/test/run_tests.py" "/home/anazar1/catkin_ws/build/test_results/jackal_description/roslaunch-check_launch_description.launch.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/anazar1/catkin_ws/build/test_results/jackal_description" "/opt/ros/noetic/share/roslaunch/cmake/../scripts/roslaunch-check -o \"/home/anazar1/catkin_ws/build/test_results/jackal_description/roslaunch-check_launch_description.launch.xml\" \"/home/anazar1/catkin_ws/src/jackal/jackal_description/launch/description.launch\" ")
-set_tests_properties(_ctest_jackal_description_roslaunch-check_launch_description.launch PROPERTIES  _BACKTRACE_TRIPLES "/opt/ros/noetic/share/catkin/cmake/test/tests.cmake;160;add_test;/opt/ros/noetic/share/roslaunch/cmake/roslaunch-extras.cmake;66;catkin_run_tests_target;/home/anazar1/catkin_ws/src/jackal/jackal_description/CMakeLists.txt;8;roslaunch_add_file_check;/home/anazar1/catkin_ws/src/jackal/jackal_description/CMakeLists.txt;0;")
